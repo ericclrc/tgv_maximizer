f7b8ff33335eeac9426a60101f2e652589d33469
ericclrc <105347815+ericclrc@users.noreply.github.com>
2022-11-24 16:01:38 +0100

    Add files via upload

---
+++ source/dao/DAOhistorique.py
@@ -0,0 +1,48 @@
+from utilisateur.utilisateur import Utilisateur
+import psycopg2
+from dao.db_connection import DBConnection
+from utils.singleton import Singleton
+
+class DAOHistorique():
+    def remplir(self,type, date, origine, destination, eligible, alerter):
+        conn=None
+        cur=None
+        try:
+            ###Connexion au serveur 
+            conn=DBConnection().connection
+            cur = conn.cursor()
+            create_script='''CREATE TABLE  IF NOT EXISTS historique (id_recherche SERIAL PRIMARY KEY NOT NULL,
+                                                                    type VARCHAR(40),
+                                                                    date VARCHAR(40),
+                                                                    origine VARCHAR(40),
+                                                                    destination VARCHAR(40),
+                                                                    eligible VARCHAR(40),
+                                                                    alerter VARCHAR(40))'''
+            cur.execute(create_script)
+            cur.execute('''INSERT INTO historique(type, date, origine, destination,
+                                                   eligible, alerter)'''\
+                        '''VALUES(%(type)s,
+                                    %(date)s,
+                                    %(origine)s,
+                                    %(destination)s,
+                                    %(eligible)s,
+                                    %(alerter)s)''',
+                        {"type": type,
+                        "date" : date,
+                        "origine" : origine,
+                        "destination" : destination,
+                        "eligible" : eligible,
+                        "alerter" : alerter
+                        })
+            conn.commit()
+        except Exception as error : 
+                print(error)
+        finally : 
+            ###déconnexion + arrêt curseur
+                if cur is not None:
+                    cur.close()
+                if conn is not None :
+                    conn.close()
+
+
+
